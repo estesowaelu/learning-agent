1bcd3081216593fbaf1cf3150715f29e58922284
Tim Honeywell <tfhoneywell@gmail.com>
2012-12-11 16:28:49 -0500

    added source

---
+++ Action.java
@@ -0,0 +1,19 @@
+public class Action {
+    private int aHealth;
+    private int aStrength;
+    private int aStamina;
+    private int aSpeed;
+    private int aSanity;
+    private int aMana;
+    private int aWealth;
+
+    public Action(int h, int s1, int s2, int s3, int s4, int m, int w){
+        aHealth = h;
+        aStrength = s1;
+        aStamina = s2;
+        aSpeed = s3;
+        aSanity = s4;
+        aMana = m;
+        aWealth = w;
+    }
+}
