527d580990664da23c5000a0f0413a2fe1f1eb4d
Tim Honeywell <tfhoneywell@gmail.com>
2012-12-13 05:22:47 -0500

    add random constructor

--- Action.java
+++ Action.java
@@ -1,4 +1,7 @@
+import java.util.Random;
+
 public class Action {
+
     private int aHealth;
     private int aStrength;
     private int aStamina;
@@ -7,7 +10,22 @@ public class Action {
     private int aMana;
     private int aWealth;
 
-    public Action(int h, int s1, int s2, int s3, int s4, int m, int w){
+    public Action() {
+        Random rand = new Random();
+        int max = 10;
+        int min = -10;
+
+        aHealth = rand.nextInt(max - min + 1) + min;
+        aStrength = rand.nextInt(max - min + 1) + min;
+        aStamina = rand.nextInt(max - min + 1) + min;
+        aSpeed = rand.nextInt(max - min + 1) + min;
+        aSanity = rand.nextInt(max - min + 1) + min;
+        aMana = rand.nextInt(max - min + 1) + min;
+        aWealth = rand.nextInt(max - min + 1) + min;
+
+    }
+
+    public Action(int h, int s1, int s2, int s3, int s4, int m, int w) {
         aHealth = h;
         aStrength = s1;
         aStamina = s2;
